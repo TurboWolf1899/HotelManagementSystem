e2c5680e184d94c93ac21fc01408baf63c8d0ef3
TurboWolf1899 <rossoneri.hr1899@gmail.com>
2023-05-18 11:13:16 +0200

    Update HotelManagementSystem.cpp

--- HotelManagementSystem.cpp
+++ HotelManagementSystem.cpp
@@ -1,7 +1,74 @@
 #include <iostream>
+#include <string>
+using namespace std;
+
+
+
+void pause()
+{
+    cout << endl << "Pritisnite enter za nastavak...";
+    string dummy;
+    cin.ignore();
+    getline(cin, dummy);
+}
 
 int main()
 {
-    std::cout << "Dobrodošli u HMS!\n";
-}
+    int izbor;
+    while (1)
+    {
+        system("cls");
+        cout << "Dobrodosli u Hotel Mol\n";
+        cout << "1. Unos soba" << endl;
+        cout << "2. Ispis soba" << endl;
+        cout << "3. Upis gostiju" << endl;
+        cout << "4. Ispis gostiju" << endl;
+        cout << "5. Ispis slobodnih soba" << endl;
+        cout << "6. Brisanje gostiju" << endl;
+        cout << "7. Uredi sobu" << endl;
+        cout << "8. Izlaz iz programa" << endl;
+        cout << "vas odabir: ";
+        cin >> izbor;
+        switch (izbor){
+        case 1:
+        {
 
+        }
+
+        case 2:
+        {
+
+        }
+        case 3:
+        {
+
+        }
+        case 4:
+        {
+
+        }
+        case 5:
+        {
+
+        }
+        case 6:
+        {
+
+        }
+        case 7:
+        {
+
+        }
+
+        case 8:
+        {
+            cout << "Izlaz iz programa!";
+            break;
+        }
+        default :
+            cout << "Krivi unos!";
+        }
+        pause();
+    }
+    return 0;
+}
